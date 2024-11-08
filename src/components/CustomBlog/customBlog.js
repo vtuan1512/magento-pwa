af3a1e7bdf92ca3440b982e0d75ff84eb89e23c4
vutuan <vutuan@email.com>
2024-11-08 17:03:47 +0700

    magento-pwa-2

--- src/components/CustomBlog/customBlog.js
+++ src/components/CustomBlog/customBlog.js
@@ -1,14 +1,8 @@
 import React from "react";
 import CmsBlockGroup from "@magento/venia-ui/lib/components/CmsBlock";
 const CustomBlog = () => {
-    const h1style = {
-        textAlign: "center",
-        margin: "1rem"
-    };
-
     return (
-        <div style={{ marginBottom: '300px' }}>
-            <h1 style={h1style}>Hello, this is custom Home Page.</h1>
+        <div>
             <CmsBlockGroup identifiers={['test-block']} />
         </div>
     );
